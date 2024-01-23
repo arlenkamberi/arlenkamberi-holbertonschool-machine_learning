c2fb55590ccce2da606f073559e9e5ff4c91320e
Arlen Kamberi <arlenkamberi@gmail.com>
2024-01-23 16:24:19 +0000

    kk

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+""" that calculates the shape of a matrix"""
+
 
 def matrix_shape(matrix):
+    """returns the shape of a multi_diantional matrix"""
     matrix_shape = []
     while type(matrix) is list:
         matrix_shape.append(len(matrix))
         matrix = matrix[0]
 
     return matrix_shape
-    
